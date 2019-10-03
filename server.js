b7ab2a2282e802a673421d0bdec706301ba32a0a
Matt Garcia <mattrg1105@gmail.com>
2019-10-02 20:48:41 -0700

    served up front end from server

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = process.env.PORT || 3000;
 const {syncAndSeed, models } = require('./db');
 const { Student, School } = models;
+
+app.use('/dist/main.js', express.static(path.join(__dirname, 'dist/main.js')));
+
+app.get('/', (req, res, next)=>{
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 app.get('/api/students', (req, res, next)=>{
   Student.findAll()
